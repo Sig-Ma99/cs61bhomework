91248d3fe157112e489ce6264dd69a6f7bfe5cc3
Yunhe <marvelousmyh@126.com>
2023-05-16 14:08:23 +0100

    completed project0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
     public double yyVel;
     public double mass;
     public String imgFileName;
-    static final double constantG = 6.67e-11;
+    private static final double constantG = 6.67e-11;
 
 
     public Planet(double xP, double yP, double xV,
